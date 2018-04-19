836b14d30b00411f66724c11952403fe5493abe8
francisco.tln <francisco.tln@outlook.com>
2018-04-20 00:02:39 +0200

    missing semi-colons

--- scripts/update-readme.js
+++ scripts/update-readme.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 const path = require('path');
 const { minify } = require('uglify-es');
-const gzipSize = require('gzip-size')
+const gzipSize = require('gzip-size');
 
 const main = require.resolve(path.join(__dirname, '..'));
 const { code: minified } = minify(
   fs.readFileSync(main, 'utf-8'),
-  { toplevel: true },
+  { toplevel: true }
 );
 
 const readme = path.join(__dirname, '../readme.md');
@@ -14,5 +14,5 @@ const data = fs.readFileSync(readme, 'utf-8');
 const updated = data.replace(
   /<span class="weight">(.*?)<\/span>/,
   `<span class="weight">${gzipSize.sync(minified)}</span>`
-)
-fs.writeFileSync(readme, updated)
+);
+fs.writeFileSync(readme, updated);
